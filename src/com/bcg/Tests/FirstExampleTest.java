89f0a2a8d39a623dd1bb5ffdfbe3ea36c098d2c6
sowjanyag2247 <sowjanya.guddeti@mobileprogramming.com>
2020-09-29 11:56:17 +0530

    not added files

--- src/com/bcg/Tests/FirstExampleTest.java
+++ src/com/bcg/Tests/FirstExampleTest.java
@@ -80,7 +80,7 @@ public class FirstExampleTest {
 
 	  @AfterClass(alwaysRun = true)
 	  public void tearDown() throws Exception {
-	    //driver.quit();
+	    driver.quit();
 	    String verificationErrorString = verificationErrors.toString();
 	    if (!"".equals(verificationErrorString)) {
 	      fail(verificationErrorString);
